7da1b01a1a5863e4f73b95aa4782b9137ff115cf
Sergio Marques <sergio@unicamp.br>
2022-11-06 23:32:47 -0300

    'inicial'

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http')
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type': 'text/html; charset=utf-8'});
+    res.end('<html><head>Teste</head>'+ 
+            '<body><h1>Alo turma<br>tudo bem<br></h1></body>'+
+            '</html>');
+}).listen(3001);
+
+console.log('Control C para parar...')
